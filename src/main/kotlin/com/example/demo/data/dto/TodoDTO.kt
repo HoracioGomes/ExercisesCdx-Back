00a7a2c414e6f54e55661c450bfdd902fb6ff770
Horacio <oicaroh64@gmail.com>
2023-10-06 17:09:11 -0300

    Date adjustments

--- src/main/kotlin/com/example/demo/data/dto/TodoDTO.kt
+++ src/main/kotlin/com/example/demo/data/dto/TodoDTO.kt
@@ -1,9 +1,11 @@
 package com.example.demo.data.dto
 
+import com.fasterxml.jackson.annotation.JsonAlias
 import java.time.LocalDateTime
 
 class TodoDTO(
         var titulo: String,
         var descricao: String,
+        @JsonAlias("date")
         var dataAgendada: String,
 )
--- src/main/kotlin/com/example/demo/domain/services/TodoService.kt
+++ src/main/kotlin/com/example/demo/domain/services/TodoService.kt
@@ -7,16 +7,22 @@ import com.example.demo.domain.services.exceptions.ResourceNotFoundException
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.dao.EmptyResultDataAccessException
 import org.springframework.stereotype.Service
+import java.time.LocalDate
 import java.time.LocalDateTime
+import java.time.LocalTime
 import java.time.ZoneId
+import java.time.format.DateTimeFormatter
 
 @Service
 class TodoService {
     @Autowired
     private lateinit var repository: TodoRepository
     fun save(dto: TodoDTO): Todo {
+        val formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd")
+        val localDate = LocalDate.parse(dto.dataAgendada, formatter)
+
         val todo = Todo(id = null, titulo = dto.titulo, descricao = dto.descricao,
-                dataAgendada = LocalDateTime.parse(dto.dataAgendada),
+                dataAgendada = LocalDateTime.of(localDate, LocalTime.now()),
                 concluida = false)
         return repository.save(todo)
     }
